88aff8122f056145299ab0ab6f6ada6711c9f552
a595848474 <a595848474@gmail.com>
2016-04-11 22:47:17 -0400

    Added

---
+++ STL_loader.py
@@ -0,0 +1,73 @@
+"""
+load STL-10 binary data set
+"""
+import gzip
+import tarfile
+import numpy as np
+
+# from theano import function as T_function
+# import theano.tensor as T
+
+import stl10_input
+
+train_X_path = "data/stl10_binary/train_X.bin"
+train_y_path = "data/stl10_binary/train_y.bin"
+unlabeled_X_path = "data/stl10_binary/unlabeled_X.bin"
+test_X_path = "data/stl10_binary/test_X.bin"
+test_y_path = "data/stl10_binary/test_y.bin"
+
+def unzip_data():
+    """
+    unzip the .gz file to "data/" directory, then we will get the paths as above
+    :return:
+    """
+    tarfile.open("data/stl10_binary.tar.gz", 'r:gz').extractall("data/")
+
+def load_labled_data(grayscale = False):
+    """
+    The value of labels should start from 0
+    Do we need to normalize the data to be between 0-1? no need currently, otherwise double value needs more memory
+    Grayscale the images
+    :return:
+    """
+    train_inputs = stl10_input.read_all_images(train_X_path)
+    train_labels = stl10_input.read_labels(train_y_path)
+    test_inputs = stl10_input.read_all_images(test_X_path)
+    test_labels = stl10_input.read_labels(test_y_path)
+    if grayscale:
+        return grayScaler(train_inputs)/255.0, train_labels - 1, grayScaler(test_inputs)/255.0, test_labels - 1
+    else:
+        return train_inputs, train_labels - 1, test_inputs, test_labels - 1
+
+def load_unlabeld_data():
+    data = stl10_input.read_all_images(unlabeled_X_path)
+    return data
+
+# images = stl10_input.read_labels(train_y_path)
+# print images.shape
+
+# new_images = np.dot(images, np.array([0.299, 0.587, 0.114]))
+# print new_images.shape
+
+def grayScaler(data):
+    """
+    the shape of data should be m*N*N*3, where m is
+    the number of observations, N is the size of each image
+    GrayScale = 0.299R + 0.587G + 0.114B, which is the formula
+    to transform RGB to gray scale
+    :param data:
+    :return:
+    """
+    return np.dot(data, np.array([0.299, 0.587, 0.114]))
+
+# train_inputs, train_labels, test_inputs, test_labels = load_labled_data(grayscale=True)
+#
+# stl10_input.plot_image(train_inputs[0])
+#
+# stl10_input.plot_image(np.transpose(train_inputs[0])[0])
+
+# [np.transpose(x) for x in train_inputs]
+
+# print train_inputs.shape
+
+
---
+++ testKeras.py
@@ -0,0 +1,58 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Convolution2D, MaxPooling2D, Flatten
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from sklearn import cross_validation
+
+
+
+from STL_loader import load_labled_data
+
+model = Sequential()
+
+model.add(Convolution2D(16, 11, 11, border_mode="valid", input_shape=(1, 96, 96)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(32, 10, 10))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(64, 5, 5))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(Flatten())
+model.add(Dense(1000))
+model.add(Activation("relu"))
+model.add(Dense(100))
+model.add(Activation("relu"))
+
+model.add(Dense(10))
+model.add(Activation("softmax"))
+
+sgd = SGD(lr=0.1)
+model.compile(loss="msle", optimizer=sgd)
+
+train_inputs, train_labels, test_inputs, test_labels = load_labled_data(grayscale=True)
+
+
+# convert class vactors to binary class matrices
+train_inputs = train_inputs.reshape(len(train_inputs),1,96,96)
+train_labels = np_utils.to_categorical(train_labels, 10)
+
+# test_inputs = test_inputs.reshape(len(test_inputs), 1, 96, 96)
+# test_labels = np_utils.to_categorical(test_labels, 10)
+
+cv = cross_validation.KFold(len(train_labels), n_folds = 5, shuffle=True)
+
+for trainCV, validCV in cv:
+    cv_train_inputs = train_inputs[trainCV]
+    cv_train_labels = train_labels[trainCV]
+    cv_valid_inputs = train_inputs[validCV]
+    cv_valid_labels = train_labels[validCV]
+    model.clear_previous()
+    model.fit(cv_train_inputs, cv_train_labels,
+          batch_size=1000, nb_epoch=1, verbose=1, show_accuracy=True, validation_data=(cv_valid_inputs, cv_valid_labels))
+
+# model.fit(train_inputs, train_labels,
+#           batch_size=20, nb_epoch=20, verbose=1, show_accuracy=True, validation_data=(test_inputs, test_labels))
