65c35217f539f1bdd48063cd81b86947647b8869
a595848474 <a595848474@gmail.com>
2016-04-12 00:45:27 -0400

    Modifed cross validation

--- testKeras.py
+++ testKeras.py
@@ -8,30 +8,32 @@ from sklearn import cross_validation
 
 from STL_loader import load_labled_data
 
-model = Sequential()
+def creat_model():
+    model = Sequential()
 
-model.add(Convolution2D(16, 11, 11, border_mode="valid", input_shape=(1, 96, 96)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Convolution2D(32, 10, 10))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Convolution2D(64, 5, 5))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(16, 11, 11, border_mode="valid", input_shape=(1, 96, 96)))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(32, 10, 10))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(64, 5, 5))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
 
-model.add(Flatten())
-model.add(Dense(1000))
-model.add(Activation("relu"))
-model.add(Dense(100))
-model.add(Activation("relu"))
+    model.add(Flatten())
+    model.add(Dense(1000))
+    model.add(Activation("relu"))
+    model.add(Dense(100))
+    model.add(Activation("relu"))
 
-model.add(Dense(10))
-model.add(Activation("softmax"))
+    model.add(Dense(10))
+    model.add(Activation("softmax"))
 
-sgd = SGD(lr=0.1)
-model.compile(loss="msle", optimizer=sgd)
+    sgd = SGD(lr=0.1)
+    model.compile(loss="msle", optimizer=sgd)
+    return model
 
 train_inputs, train_labels, test_inputs, test_labels = load_labled_data(grayscale=True)
 
@@ -52,7 +54,7 @@ for trainCV, validCV in cv:
     cv_train_labels = train_labels[trainCV]
     cv_valid_inputs = train_inputs[validCV]
     cv_valid_labels = train_labels[validCV]
-    model.clear_previous()
+    model = creat_model()
     model.fit(cv_train_inputs, cv_train_labels,
           batch_size=50, nb_epoch=20, verbose=1, show_accuracy=True, validation_data=(cv_valid_inputs, cv_valid_labels))
     i += 1
